c9222bc2176b3d3cbb0346524dde013551c1f06f
flaccid <chris@fordham-nagy.id.au>
2017-11-11 15:16:32 +1100

    Initial dev/test version of check2metrics.

---
+++ checks2metrics.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+  "bytes"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+  log "github.com/sirupsen/logrus"
+  "os"
+  "strconv"
+  "strings"
+  "time"
+
+  "github.com/russellcardullo/go-pingdom/pingdom"
+  "github.com/urfave/cli"
+)
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "checks2metrics"
+  app.Version = "v0.0.1"
+  app.Compiled = time.Now()
+  app.Copyright = "(c) 2016 Chris Fordham"
+  app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Chris Fordham",
+			Email: "chris@fordham-nagy.id.au",
+		},
+	}
+  app.Usage = "Ship Pingdom checks to LambStatus metrics."
+  app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "pingdom-user",
+      EnvVar: "PINGDOM_USERNAME",
+		},
+		cli.StringFlag{
+			Name:  "pingdom-pass",
+      EnvVar: "PINGDOM_PASSWORD",
+		},
+    cli.StringFlag{
+			Name:  "pingdom-api-key",
+      EnvVar: "PINGDOM_API_KEY",
+		},
+    cli.StringFlag{
+			Name:  "lambstatus-endpoint",
+      EnvVar: "LAMBSTATUS_ENDPOINT_URL",
+		},
+    cli.StringFlag{
+			Name:  "lambstatus-api-key",
+      EnvVar: "LAMBSTATUS_API_KEY",
+		},
+    cli.StringFlag{
+      Name:  "check-to-metric-map",
+      EnvVar: "CHECK_TO_METRIC_MAP",
+    },
+	}
+
+  app.Action = func(c *cli.Context) error {
+    log.WithFields(log.Fields{
+      "lambstatus endpoint url": c.String("lambstatus-endpoint"),
+      "check2metric map": c.String("check-to-metric-map"),
+    }).Info("starting")
+
+    pClient := pingdom.NewClient(c.String("pingdom-user"),
+                                 c.String("pingdom-pass"),
+                                 c.String("pingdom-api-key"))
+    checkToMetricMap := c.String("check-to-metric-map")
+
+    // split the map by comma
+    mappings := strings.Split(checkToMetricMap, ",")
+    log.Info("processing ", len(mappings), " mappings")
+
+    // iterate through each mapping
+    for i := range mappings {
+      mapping := strings.Split(mappings[i], ":")
+      log.WithFields(log.Fields{
+        "pingdom check id": mapping[0],
+        "lambstatus metric id": mapping[1],
+      }).Info("processing mapping ", i)
+
+      checkId, err := strconv.Atoi(mapping[0])
+      metricId := mapping[1]
+
+      // get details for the check
+      checkDetails, _ := pClient.Checks.Read(checkId)
+      log.Info("Check Details: %+v\n", checkDetails)
+      lastResponseTime := checkDetails.LastResponseTime
+      lastTestTime := checkDetails.LastTestTime
+
+      log.Info(fmt.Sprintf("Last Response Time: %vms", lastResponseTime))
+      log.Info(fmt.Sprintf("Last Test Time: %v ", lastTestTime))
+
+      t := time.Unix(lastTestTime, 0).Add(1).UTC().Format(time.RFC3339Nano)
+
+      var jsonPayLoad = []byte("{\""+metricId+`": [{"timestamp": "`+t+`", "value": `+fmt.Sprintf("%v", lastResponseTime)+`}]}`)
+      log.Info("JSON payload:", string(jsonPayLoad[:]))
+
+      url := c.String("lambstatus-endpoint") + "/prod/v0/metrics/data"
+      log.Info("POST:", url)
+
+      // send to lambstatus
+      req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayLoad))
+      req.Header.Set("x-api-key", c.String("lambstatus-api-key"))
+      req.Header.Set("Content-Type", "application/json")
+
+      client := &http.Client{}
+      resp, err := client.Do(req)
+      if err != nil {
+        panic(err)
+      }
+      defer resp.Body.Close()
+
+      log.Info("response Status:", resp.Status)
+      log.Info("response Headers:", resp.Header)
+      body, _ := ioutil.ReadAll(resp.Body)
+      log.Info("response Body:", string(body))
+    }
+
+    return nil
+  }
+
+  app.Run(os.Args)
+}
