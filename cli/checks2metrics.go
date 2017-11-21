65a9981ee479e1836e713a332faa9ea672dfc15c
Chris Fordham <chris@fordham-nagy.id.au>
2017-11-21 18:34:19 +1100

    Fix all the indentation in the go files.

--- cli/checks2metrics.go
+++ cli/checks2metrics.go
@@ -1,95 +1,95 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "time"
+	"fmt"
+	"os"
+	"time"
 
-  factory "github.com/flaccid/lambstatus-pingdom/factory"
-  "github.com/urfave/cli"
+	factory "github.com/flaccid/lambstatus-pingdom/factory"
+	"github.com/urfave/cli"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "checks2metrics"
-  app.Version = "v0.0.1"
-  app.Compiled = time.Now()
-  app.Copyright = "(c) 2016 Chris Fordham"
-  app.Authors = []cli.Author{
+	app := cli.NewApp()
+	app.Name = "checks2metrics"
+	app.Version = "v0.0.1"
+	app.Compiled = time.Now()
+	app.Copyright = "(c) 2016 Chris Fordham"
+	app.Authors = []cli.Author{
 		cli.Author{
 			Name:  "Chris Fordham",
 			Email: "chris@fordham-nagy.id.au",
 		},
 	}
-  app.Usage = "Ship Pingdom checks to LambStatus metrics."
-  app.Flags = []cli.Flag{
+	app.Usage = "Ship Pingdom checks to LambStatus metrics."
+	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "pingdom-user",
-      EnvVar: "PINGDOM_USERNAME",
+			Name:   "pingdom-user",
+			EnvVar: "PINGDOM_USERNAME",
 		},
 		cli.StringFlag{
-			Name:  "pingdom-pass",
-      EnvVar: "PINGDOM_PASSWORD",
+			Name:   "pingdom-pass",
+			EnvVar: "PINGDOM_PASSWORD",
 		},
-    cli.StringFlag{
-			Name:  "pingdom-api-key",
-      EnvVar: "PINGDOM_API_KEY",
+		cli.StringFlag{
+			Name:   "pingdom-api-key",
+			EnvVar: "PINGDOM_API_KEY",
 		},
-    cli.StringFlag{
-			Name:  "lambstatus-endpoint",
-      EnvVar: "LAMBSTATUS_ENDPOINT_URL",
+		cli.StringFlag{
+			Name:   "lambstatus-endpoint",
+			EnvVar: "LAMBSTATUS_ENDPOINT_URL",
 		},
-    cli.StringFlag{
-			Name:  "lambstatus-api-key",
-      EnvVar: "LAMBSTATUS_API_KEY",
+		cli.StringFlag{
+			Name:   "lambstatus-api-key",
+			EnvVar: "LAMBSTATUS_API_KEY",
 		},
-    cli.StringFlag{
-      Name:  "check-to-metric-map",
-      EnvVar: "CHECK_TO_METRIC_MAP",
-    },
-    cli.BoolFlag{
-      Name:  "debug",
-      Usage: "set debug log level",
-    },
+		cli.StringFlag{
+			Name:   "check-to-metric-map",
+			EnvVar: "CHECK_TO_METRIC_MAP",
+		},
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "set debug log level",
+		},
 	}
 
-  app.Action = func(c *cli.Context) error {
-    // option validation
-    if len(c.String("lambstatus-endpoint")) < 1 {
-      fmt.Println("--lambstatus-endpoint is missing, please provide a value")
-      os.Exit(1)
-    }
-    if len(c.String("lambstatus-api-key")) < 1 {
-      fmt.Println("--lambstatus-api-key is missing, please provide a value")
-      os.Exit(1)
-    }
-    if len(c.String("pingdom-user")) < 1 {
-      fmt.Println("--pingdom-user is missing, please provide a value")
-      os.Exit(1)
-    }
-    if len(c.String("pingdom-pass")) < 1 {
-      fmt.Println("--pingdom-pass is missing, please provide a value")
-      os.Exit(1)
-    }
-    if len(c.String("pingdom-api-key")) < 1 {
-      fmt.Println("--pingdom-api-key is missing, please provide a value")
-      os.Exit(1)
-    }
-    if len(c.String("check-to-metric-map")) < 1 {
-      fmt.Println("--pingdom-user is missing, please provide a value")
-      os.Exit(1)
-    }
+	app.Action = func(c *cli.Context) error {
+		// option validation
+		if len(c.String("lambstatus-endpoint")) < 1 {
+			fmt.Println("--lambstatus-endpoint is missing, please provide a value")
+			os.Exit(1)
+		}
+		if len(c.String("lambstatus-api-key")) < 1 {
+			fmt.Println("--lambstatus-api-key is missing, please provide a value")
+			os.Exit(1)
+		}
+		if len(c.String("pingdom-user")) < 1 {
+			fmt.Println("--pingdom-user is missing, please provide a value")
+			os.Exit(1)
+		}
+		if len(c.String("pingdom-pass")) < 1 {
+			fmt.Println("--pingdom-pass is missing, please provide a value")
+			os.Exit(1)
+		}
+		if len(c.String("pingdom-api-key")) < 1 {
+			fmt.Println("--pingdom-api-key is missing, please provide a value")
+			os.Exit(1)
+		}
+		if len(c.String("check-to-metric-map")) < 1 {
+			fmt.Println("--pingdom-user is missing, please provide a value")
+			os.Exit(1)
+		}
 
-    factory.Ship(c.String("lambstatus-endpoint"),
-         c.String("lambstatus-api-key"),
-         c.String("pingdom-user"),
-         c.String("pingdom-pass"),
-         c.String("pingdom-api-key"),
-         c.String("check-to-metric-map"),
-         c.Bool("debug"))
+		factory.Ship(c.String("lambstatus-endpoint"),
+			c.String("lambstatus-api-key"),
+			c.String("pingdom-user"),
+			c.String("pingdom-pass"),
+			c.String("pingdom-api-key"),
+			c.String("check-to-metric-map"),
+			c.Bool("debug"))
 
-    return nil
-  }
+		return nil
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
--- factory/factory.go
+++ factory/factory.go
@@ -1,94 +1,95 @@
 package factory
 
 import (
-  "bytes"
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  log "github.com/sirupsen/logrus"
-  "strconv"
-  "strings"
-  "time"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
-  "github.com/russellcardullo/go-pingdom/pingdom"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/russellcardullo/go-pingdom/pingdom"
 )
 
 func Ship(lambStatusEndpoint string,
-          lambStatusApiKey string,
-          pingdomUser string,
-          pingdomPass string,
-          pingdomApiKey string,
-          checkToMetricMap string,
-          debugMode bool) {
+	lambStatusApiKey string,
+	pingdomUser string,
+	pingdomPass string,
+	pingdomApiKey string,
+	checkToMetricMap string,
+	debugMode bool) {
 
-   log.WithFields(log.Fields{
-     "lambstatus endpoint url": lambStatusEndpoint,
-     "checks to metrics map": checkToMetricMap,
-   }).Info("starting")
+	log.WithFields(log.Fields{
+		"lambstatus endpoint url": lambStatusEndpoint,
+		"checks to metrics map":   checkToMetricMap,
+	}).Info("starting")
 
-   if debugMode {
-     log.Info("debug enabled")
-     log.SetLevel(log.DebugLevel)
-   }
+	if debugMode {
+		log.Info("debug enabled")
+		log.SetLevel(log.DebugLevel)
+	}
 
-   pClient := pingdom.NewClient(pingdomUser, pingdomPass, pingdomApiKey)
+	pClient := pingdom.NewClient(pingdomUser, pingdomPass, pingdomApiKey)
 
-   // split the map by comma
-   mappings := strings.Split(checkToMetricMap, ",")
-   log.Info(len(mappings), " checks to ship to lambstatus")
+	// split the map by comma
+	mappings := strings.Split(checkToMetricMap, ",")
+	log.Info(len(mappings), " checks to ship to lambstatus")
 
-   // iterate through each mapping
-   for i := range mappings {
-     mapping := strings.Split(mappings[i], ":")
-     checkId, err := strconv.Atoi(mapping[0])
-     metricId := mapping[1]
-     checkDetails, err := pClient.Checks.Read(checkId)
-     if err != nil {
-       log.Fatal(err)
-     }
-     log.Debug("check details: %+v\n", checkDetails)
-     lastResponseTime := checkDetails.LastResponseTime
-     lastTestTime := checkDetails.LastTestTime
+	// iterate through each mapping
+	for i := range mappings {
+		mapping := strings.Split(mappings[i], ":")
+		checkId, err := strconv.Atoi(mapping[0])
+		metricId := mapping[1]
+		checkDetails, err := pClient.Checks.Read(checkId)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Debug("check details: %+v\n", checkDetails)
+		lastResponseTime := checkDetails.LastResponseTime
+		lastTestTime := checkDetails.LastTestTime
 
-     dateStamp := time.Unix(lastTestTime, 0).Add(1).UTC().Format(time.RFC3339Nano)
-     var jsonPayLoad = []byte("{\""+metricId+`": [{"timestamp": "` + dateStamp + `", "value": ` + fmt.Sprintf("%v", lastResponseTime) + `}]}`)
+		dateStamp := time.Unix(lastTestTime, 0).Add(1).UTC().Format(time.RFC3339Nano)
+		var jsonPayLoad = []byte("{\"" + metricId + `": [{"timestamp": "` + dateStamp + `", "value": ` + fmt.Sprintf("%v", lastResponseTime) + `}]}`)
 
-     log.WithFields(log.Fields{
-       "pingdom check id": checkId,
-       "lambstatus metric id": metricId,
-       "last response time": lastResponseTime,
-       "last test time": lastTestTime,
-       "datestamp": dateStamp,
-     }).Info(i, " ", checkDetails.Name)
-     log.Debug("JSON payload:", string(jsonPayLoad[:]))
+		log.WithFields(log.Fields{
+			"pingdom check id":     checkId,
+			"lambstatus metric id": metricId,
+			"last response time":   lastResponseTime,
+			"last test time":       lastTestTime,
+			"datestamp":            dateStamp,
+		}).Info(i, " ", checkDetails.Name)
+		log.Debug("JSON payload:", string(jsonPayLoad[:]))
 
-     url := lambStatusEndpoint + "/api/v0/metrics/data"
-     log.Debug("POST: ", url)
+		url := lambStatusEndpoint + "/api/v0/metrics/data"
+		log.Debug("POST: ", url)
 
-     // send to lambstatus
-     req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayLoad))
-     req.Header.Set("x-api-key", lambStatusApiKey)
-     req.Header.Set("Content-Type", "application/json")
-     log.Debug("request", req)
+		// send to lambstatus
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayLoad))
+		req.Header.Set("x-api-key", lambStatusApiKey)
+		req.Header.Set("Content-Type", "application/json")
+		log.Debug("request", req)
 
-     client := &http.Client{}
-     resp, err := client.Do(req)
-     if err != nil {
-       panic(err)
-     }
-     defer resp.Body.Close()
-     body, err := ioutil.ReadAll(resp.Body)
-     if err != nil {
-       log.Fatal(err)
-     }
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-     log.Debug("response status: ", resp.Status)
-     log.Debug("response headers: ", resp.Header)
-     log.Debug("response body: ", string(body))
+		log.Debug("response status: ", resp.Status)
+		log.Debug("response headers: ", resp.Header)
+		log.Debug("response body: ", string(body))
 
-     if resp.StatusCode != 200 {
-       log.Error("failed to post metric: ", resp.Status, " ", string(body))
-     }
-   }
-   log.Info("all checks shipped")
+		if resp.StatusCode != 200 {
+			log.Error("failed to post metric: ", resp.Status, " ", string(body))
+		}
+	}
+	log.Info("all checks shipped")
 }
