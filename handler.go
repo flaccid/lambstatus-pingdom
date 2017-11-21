2eceac56b9ad08e5a1626b118740b48346c9399f
Chris Fordham <cfordham@scentregroup.com>
2017-11-21 13:54:51 +1100

    Update the handler to actually use lambstatus-pingdom/factory.

--- handler.go
+++ handler.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+import (
+	"log"
+	"os"
 
-func Handle(evt interface{}, ctx *runtime.Context) (string, error) {
-	return "Hello, World!", nil
+	factory "github.com/flaccid/lambstatus-pingdom/factory"
+	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+)
+
+var (
+  debugMode bool = false
+)
+
+func Handle(evt interface{}, ctx *runtime.Context) (interface{}, error) {
+	log.Println("function starting")
+
+	factory.Ship(os.Getenv("LAMBSTATUS_ENDPOINT_URL"),
+			         os.Getenv("LAMBSTATUS_API_KEY"),
+			         os.Getenv("PINGDOM_USERNAME"),
+			         os.Getenv("PINGDOM_PASSWORD"),
+			         os.Getenv("PINGDOM_API_KEY"),
+			         os.Getenv("CHECK_TO_METRIC_MAP"),
+               debugMode)
+
+	log.Println("function complete")
+
+	return "done", nil
 }
