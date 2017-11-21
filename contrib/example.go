2a63610bd446460944378582a308557aeb46accb
Chris Fordham <cfordham@scentregroup.com>
2017-11-21 13:54:09 +1100

    Add a go example file.

---
+++ contrib/example.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	factory "github.com/flaccid/lambstatus-pingdom/factory"
+)
+
+var (
+  debugMode bool = false
+)
+
+func main() {
+  factory.Ship(os.Getenv("LAMBSTATUS_ENDPOINT_URL"),
+			         os.Getenv("LAMBSTATUS_API_KEY"),
+			         os.Getenv("PINGDOM_USERNAME"),
+			         os.Getenv("PINGDOM_PASSWORD"),
+			         os.Getenv("PINGDOM_API_KEY"),
+			         os.Getenv("CHECK_TO_METRIC_MAP"),
+               debugMode)
+}
